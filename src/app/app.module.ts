94e5529b5bc19388572a0dd584b5b7cdc09c27d0
shubhraj <ashu.shubhraj@gmail.com>
2018-10-06 18:25:13 +0530

    Implemented an Angular WebApp in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+/*
+* Angular needs to know the pieces of your application fit together
+  and what other files and libraries the app requires. This information
+  is called the MetaData.
+* Some of the metadata is in the @Component decorators that you added to 
+  your component classes. Other critical metadata is in @NgModule decorators.  
+* The most important @NgModule decorator annotates the top-level AppModule class.
+* Every component must be declared in exactly one NgModule.
+* 
+*/
 
-import { AppComponent } from './app.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { MessagesComponent }    from './messages/messages.component';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
